20a455b9d421d36b85b361cecb85622a96838765
addehd <hej@adolfo.se>
2020-02-20 20:09:21 +0100

    fetch, display and search data from bikewise

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './styles/App.css';
+import Header from './components/Header.js';
+import Bikes from './components/Bikes.js'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Header/>
+      <Switch>
+        <Route path="/" exact component={ Bikes }/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ app/src/components/Bikes.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import '../styles/App.css'
+
+function Bikes() {
+  const [incidents, setIncidentData] = useState({ incidents: [] })
+  const [search, setSeach] = useState(null)
+  const [load, setLoad] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        `https://bikewise.org:443/api/v2/incidents?page=1&proximity=55.607405%2C12.998945&proximity_square=100`
+      )
+      setIncidentData(result.data)
+      setLoad(false)
+    }
+    fetchData()
+  }, [])
+
+  let searchSpace = (event)=> {
+    let keyword = event.target.value
+    setSeach({search:keyword})
+  }
+
+  const items = incidents.incidents.filter((data)=>{
+    console.log(data.title)
+    console.log(search)
+    if(search == null){
+        return data }
+    else if(data.title.toLowerCase().includes(search.search.toLowerCase()) || data.address.toLowerCase().includes(search.search.toLowerCase()) ){
+        return data
+    }
+  }).map(data=>{
+    return(
+    <div className="listed" key={data.id}>
+          <span>{data.title}</span>
+    </div>
+    )
+  })
+
+  return (
+    <div id="bikes">
+      <label>
+         <input type="text" placeholder="Search for stolen bike's" onChange={(e)=>searchSpace(e)} />
+      </label>
+      <section>{items}</section>
+    </div>
+  )
+}
+
+export default Bikes
